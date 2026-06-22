8a9eab736a5652e49e2085ce0be1a141d4712ced
Victor Ling <victorling@ionshard.com>
2026-06-22 19:21:32 -0400

    add webs

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
+const WEB = preload("uid://c8l4u2mvfe2jq")
+signal web_shot(web: Web)
 
-var new_point: Vector2 = Vector2.ZERO
+var new_point: Vector2
+var is_moving = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +15,17 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	ray_cast_2d.target_position = global_position.direction_to(get_global_mouse_position()) * 5000
 	
-	if Input.is_action_just_pressed('spin_web'):
+	if not is_moving and Input.is_action_just_pressed('spin_web'):
 		if(ray_cast_2d.is_colliding()):
-			global_position = ray_cast_2d.get_collision_point() - ray_cast_2d.get_collision_normal()
+			new_point = ray_cast_2d.get_collision_point() - ray_cast_2d.get_collision_normal()
+			is_moving = true
+			var new_web: Line2D = WEB.instantiate()
+			new_web.add_point(global_position)
+			new_web.add_point(new_point)
+			web_shot.emit(new_web)
+	
+	if((global_position - new_point).length_squared() < 1):
+		is_moving = false
+			
+	if(is_moving):
+		global_position = global_position.move_toward(new_point, 500 * delta)
--- game/web/web.gd
+++ game/web/web.gd
@@ -1,7 +1,6 @@
-extends Node2D
+extends Line2D
+class_name Web
 
-@onready var player: Area2D = %Player
-@onready var webs: Node = %Webs
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +10,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_player_web_shot(web: Web) -> void:
-	webs.add_child(web)
