899790f97b54a420b717de8d5f18137be3709505
Victor Ling <victorling@ionshard.com>
2026-06-25 20:30:01 -0400

    air web level

--- resources/level_switcher.gd
+++ resources/level_switcher.gd
@@ -4,6 +4,7 @@ var levels = [
 	"res://game/levels/level_1.tscn",
 	"res://game/levels/level_2.tscn",
 	"res://game/levels/multi_shot.tscn",
+	"res://game/levels/air_web.tscn",
 	"res://game/levels/trick_shot.tscn",
 ]
 var currentLevel = 0
